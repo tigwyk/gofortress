352544e1cf5ab35221f32cffa07d6b9a6416e013
Lee Ingram <lee@soylentcola.com>
2016-11-17 14:31:15 +0000

    Set up basic inheritance in structs and added an NPC class

--- gofortress.go
+++ gofortress.go
@@ -3,29 +3,41 @@ package main
 import (
 	"fmt"
 	"github.com/fatih/color"
+	"time"
 )
 
-var newPlayer player
+var newPlayer *Player
+
 var yellow = color.New(color.FgYellow).SprintFunc()
 var red = color.New(color.FgRed).SprintFunc()
 var cyan = color.New(color.FgCyan).SprintFunc()
 
-type player struct {
+var tSecond = time.NewTicker(time.Second)
+
+type Living struct {
 	name         string
 	age          int
 	gender       string
 	gold_on_hand int
-	gold_in_bank int
 	health       int
 	race         string
+	x            int
+	y            int
+	z            int
+}
+
+type Player struct {
+	Living
+	gold_in_bank int
 	class        string
 	right_hand   string
 	left_hand    string
 	possessive   string
 	objective    string
-	x            int
-	y            int
-	z            int
+}
+
+type NPC struct {
+	Living
 }
 
 func new_game() {
@@ -35,7 +47,7 @@ func new_game() {
 	fmt.Print("What is your name? ")
 	playerName := ""
 	fmt.Scanln(&playerName)
-	fmt.Print("What is your gender? ")
+	fmt.Print("Gender? ")
 	playerGender := ""
 	fmt.Scanln(&playerGender)
 	switch playerGender {
@@ -49,13 +61,35 @@ func new_game() {
 		obj = "them"
 		pos = "their"
 	}
-	newPlayer = player{playerName, 25, playerGender, 0, 0, 100, "human", "adventurer", "nothing", "nothing", pos, obj, 0, 0, 0}
+	newPlayer = new(Player)
+	newPlayer.name = playerName
+	newPlayer.age = 25
+	newPlayer.gender = playerGender
+	newPlayer.gold_on_hand = 0
+	newPlayer.gold_in_bank = 0
+	newPlayer.health = 100
+	newPlayer.race = "human"
+	newPlayer.class = "adventurer"
+	newPlayer.right_hand = "nothing"
+	newPlayer.left_hand = "nothing"
+	newPlayer.possessive = pos
+	newPlayer.objective = obj
+	newPlayer.x = 0
+	newPlayer.y = 0
+	newPlayer.z = 0
 	fmt.Printf("%s the %s %s has %s.\n", yellow(newPlayer.name), cyan(newPlayer.race), cyan(newPlayer.class), red("entered the world"))
+}
+
+func tick() {
+	fmt.Println("tick")
 }
 
 func main() {
 	new_game()
 	fmt.Println(newPlayer.name, "begins", newPlayer.possessive, "adventure.")
 	// Initiate game loop
-
+	for {
+		tick()
+		<-tSecond.C
+	}
 }
